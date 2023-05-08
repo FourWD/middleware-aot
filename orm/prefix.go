616e79fab4d8b5224df503e036fb234866022c31
p-amu <pattanapong.amu@gmail.com>
2023-05-08 16:37:06 +0700

    update orm
    promotion type amount fields add and settlement table add

--- orm/prefix.go
+++ orm/prefix.go
@@ -8,6 +8,5 @@ type Prefix struct {
 	ID string `db:"id" json:"id" gorm:"type:varchar(36);primary_key;"`
 	orm.GormModel
 
-	Code string `db:"code" json:"code" gorm:"type:varchar(20)"`
 	Name string `db:"name" json:"name" gorm:"type:varchar(150)"`
 }
---
+++ orm/settlement.go
@@ -0,0 +1,14 @@
+package orm
+
+import (
+	orm "github.com/HinekoTech/middleware/orm"
+)
+
+type Settlement struct {
+	ID string `db:"id" json:"id" gorm:"type:varchar(2);primary_key;"`
+	orm.GormModel
+
+	CounterID      string `db:"counter_id" json:"counter_id" gorm:"type:varchar(36);"`
+	StartingTime   string `db:"starting_time" json:"starting_time" gorm:"type:varchar(50) ; comment:'เวลาเริ่ม' "`
+	SettlementTime string `db:"settlement_time" json:"settlement_time" gorm:"type:varchar(50) ; comment:'เวลาตัดจ่ายยอด'"`
+}
