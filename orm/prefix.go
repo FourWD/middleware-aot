a7c5e61e006ee72a0202305eac6164f0abebe2f5
Pattanapong <pattanapong.amu@gmail.com>
2023-05-11 17:46:30 +0700

    update  prefix
    row order in prefix

--- orm/prefix.go
+++ orm/prefix.go
@@ -8,5 +8,6 @@ type Prefix struct {
 	ID string `db:"id" json:"id" gorm:"type:varchar(2);primary_key;"`
 	orm.GormModel
 
-	Name string `db:"name" json:"name" gorm:"type:varchar(150)"`
+	Name     string `db:"name" json:"name" gorm:"type:varchar(150)"`
+	RowOrder int    `db:"row_order" json:"row_order" gorm:"type:int"`
 }
