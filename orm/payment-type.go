e83ab7061d5e56ef6d571e5b1371ac753bb7c00d
Pattanapong <pattanapong.amu@gmail.com>
2023-05-11 13:09:10 +0700

    paymenttype
    add promotion type & promotion amount

--- orm/payment-type.go
+++ orm/payment-type.go
@@ -8,7 +8,9 @@ type PaymentType struct { //no CRUD 01 = เงินสด 02 = creditcard 03= 
 	ID string `db:"id" json:"id" gorm:"type:varchar(2);primary_key;"`
 	orm.GormModel
 
-	Name     string `db:"name" json:"name" gorm:"type:varchar(150)"`
-	IsWht    bool   `db:"is_wht" json:"is_wht" gorm:"default:0; type:tinyint(1); comment:'มีwht?' "`
-	RowOrder int    `db:"row_order" json:"row_order" gorm:"type:int"`
+	Name            string `db:"name" json:"name" gorm:"type:varchar(150)"`
+	IsWht           bool   `db:"is_wht" json:"is_wht" gorm:"default:0; type:tinyint(1); comment:'มีwht?' "`
+	PromotionType   string `db:"promotion_type" json:"promotion_type" gorm:"type:varchar(5);"`
+	PromotionAmount int    `db:"promotion_amount" json:"promotion_amount" gorm:"type:int(5);"`
+	RowOrder        int    `db:"row_order" json:"row_order" gorm:"type:int"`
 }
