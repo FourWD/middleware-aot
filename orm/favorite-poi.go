ce1ea82ecad45fa995f01a9f9be7b6b1204d8b28
Pattanapong <pattanapong.amu@gmail.com>
2023-05-11 11:53:09 +0700

    update fav-poi
    fleet client id

--- orm/favorite-poi.go
+++ orm/favorite-poi.go
@@ -8,6 +8,7 @@ type FavoritePoi struct { // ปลายทางใช้แสดงที่
 	ID string `db:"id" json:"id" gorm:"type:varchar(36);primary_key;"`
 	orm.GormModel
 
-	PoiID    string `db:"poi_id" json:"poi_id" gorm:"type:varchar(36);"`
-	RowOrder int    `db:"row_order" json:"row_order" gorm:"type:int"`
+	PoiID         string `db:"poi_id" json:"poi_id" gorm:"type:varchar(36);"`
+	FleetClientID string `db:"fleet_client_id"  json:"fleet_client_id" gorm:"type:varchar(4);"`
+	RowOrder      int    `db:"row_order" json:"row_order" gorm:"type:int"`
 }
