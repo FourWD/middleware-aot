e62cd4c3b0379109340480f0d9ec312430d6c6ba
Pattanapong <pattanapong.amu@gmail.com>
2023-05-17 15:08:25 +0700

    recent db

--- orm/counter.go
+++ orm/counter.go
@@ -8,7 +8,6 @@ type Counter struct { // จุดขายหน้าเค้าเตอร�
 	ID string `db:"id" json:"id" gorm:"type:varchar(36);primary_key;"`
 	orm.GormModel
 
-	Name   string `db:"name" json:"name" gorm:"type:varchar(150)"`
-	IP     string `db:"ip" json:"ip" gorm:"type:varchar(20)"`
-	Remark string `db:"remark" json:"remark" gorm:"type:varchar(200)"`
+	Name string `db:"name" json:"name" gorm:"type:varchar(150)"`
+	IP   string `db:"ip" json:"ip" gorm:"type:varchar(20)"`
 }
