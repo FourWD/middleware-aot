89654ab14cb94551f4d10b1c9763b28f2a1d017f
Narongrit Mekpho <narongritjamenrtmp@gmail.com>
2023-06-21 18:28:18 +0700

    edit cancle-type

--- orm/cancel-type.go
+++ orm/cancel-type.go
@@ -4,7 +4,7 @@ import (
 	orm "github.com/FourWD/middleware/orm"
 )
 
-type CanCelType struct {
+type CancelType struct {
 	ID string `db:"id" json:"id" gorm:"type:varchar(2);primary_key;"`
 	orm.GormModel
 
