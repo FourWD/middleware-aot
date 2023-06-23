7ec7aef46066fe737058c7ff8e13666d21016b49
ManassaweeW <winra159@gmail.com>
2023-06-23 16:14:18 +0700

    Update fleet.go
    update field
    RefName
    Address

--- orm/fleet.go
+++ orm/fleet.go
@@ -12,5 +12,7 @@ type Fleet struct {
 	BusinessTypeID string `db:"business_type_id" json:"business_type_id" gorm:"type:varchar(36)"`
 
 	RefCode  string `db:"ref_code" json:"ref_code" gorm:"type:varchar(50)"`
+	RefName  string `db:"ref_code" json:"ref_name" gorm:"type:varchar(50)"`
 	RowOrder int    `db:"row_order" json:"row_order" gorm:"type:int"`
+	Address  string `db:"address" json:"address" gorm:"type:varchar(150)"`
 }
