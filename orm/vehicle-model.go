1f33543492040631593986d89235462c8368b8ca
Narongrit Mekpho <narongritjamenrtmp@gmail.com>
2023-06-27 15:37:30 +0700

    update orm

--- orm/vehicle-model.go
+++ orm/vehicle-model.go
@@ -6,7 +6,7 @@ type VehicleModel struct {
 	ID string `db:"id" json:"id" gorm:"type:varchar(2);primary_key;"`
 	orm.GormModel
 
-	VehicleBrandID string `db:"vehicle_brand_id" json:"vehicle_brand_id"`
+	VehicleBrandID string `db:"vehicle_brand_id" json:"vehicle_brand_id" gorm:"type:varchar(2);primary_key;"`
 	VehicleTypeID  string `db:"vehicle_type_id" json:"vehicle_type_id"`
 
 	Name     string `db:"name" json:"type_name"`
