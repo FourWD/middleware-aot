54c7d7f134f5926efbd6439ed931aec538b86c30
Narongrit Mekpho <narongritjamenrtmp@gmail.com>
2023-07-12 11:22:37 +0700

    Revert "update orm"
    This reverts commit 1784f8cbaaea0d0914e67db386572be4d7f25e39.

--- orm/vehicle-sub-model.go
+++ orm/vehicle-sub-model.go
@@ -9,8 +9,8 @@ type VehicleSubModel struct {
 	VehicleSourceID string `db:"vehicle_source_id" json:"vehicle_source_id" gorm:"type:varchar(36)"`
 	VehicleModelID  string `db:"vehicle_model_id" json:"vehicle_model_id" gorm:"type:varchar(36)"`
 
-	Name     string `db:"name" json:"type_name"  gorm:"type:varchar(50)"`
-	NameEn   string `db:"name_en" json:"type_name_en" gorm:"type:varchar(50)"`
+	Name     string `db:"name" json:"type_name"`
+	NameEn   string `db:"name_en" json:"type_name_en"`
 	RowOrder int    `db:"row_order" json:"row_order" gorm:"type:int"`
 	Image    string `db:"image" json:"image"`
 }
