3860521012420dfba626fa3884f5b892b5898721
Narongrit Mekpho <narongritjamenrtmp@gmail.com>
2023-07-12 11:21:24 +0700

    update orm

--- orm/vehicle-sub-model.go
+++ orm/vehicle-sub-model.go
@@ -6,8 +6,8 @@ type VehicleSubModel struct {
 	ID string `db:"id" json:"id" gorm:"type:varchar(2);primary_key;"`
 	orm.GormModel
 
-	VehicleSourceID string `db:"vehicle_source_id" json:"vehicle_source_id"`
-	VehicleModelID  string `db:"vehicle_model_id" json:"vehicle_model_id"`
+	VehicleSourceID string `db:"vehicle_source_id" json:"vehicle_source_id" gorm:"type:varchar(36)"`
+	VehicleModelID  string `db:"vehicle_model_id" json:"vehicle_model_id" gorm:"type:varchar(36)"`
 
 	Name     string `db:"name" json:"type_name"  gorm:"type:varchar(50)"`
 	NameEn   string `db:"name_en" json:"type_name_en" gorm:"type:varchar(50)"`
